db748c8dfa80e25db12150e843ba4df21cc723d9
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2023-08-28 00:32:24 +0530

    start practicing spring integration with hibernate

---
+++ springHibernate/src/main/java/com/learnSpringHibernate/springHibernate/springHibernate.java
@@ -0,0 +1,7 @@
+package com.learnSpringHibernate.springHibernate;
+
+public class springHibernate {
+    public static void main(String[] args) {
+        System.out.println("Spring hibernate running");
+    }
+}
